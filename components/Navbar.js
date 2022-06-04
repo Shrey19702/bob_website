d8db1c932cbad8d41e0bf63f02a6021aaa1121fd
shrey19702 <shrey19702@gmail.com>
2022-06-04 21:04:18 +0530

    add components folder

---
+++ components/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <>
+            <h1>NAVBAR</h1>
+        </>
+    );
+}
+ 
+export default Navbar;
---
+++ pages/hello.js
@@ -0,0 +1,7 @@
+const Hello = () => {
+    return ( <h1>
+        Hello TITU
+    </h1> );
+}
+ 
+export default Hello;
