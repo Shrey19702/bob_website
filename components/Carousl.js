8d3517f49ee7109370dc252dcae14ad58c422f7d
Tushar Mishra <79136754+hunter1290@users.noreply.github.com>
2022-06-08 00:22:27 +0530

    Carousl

---
+++ components/Carousl.js
@@ -0,0 +1,95 @@
+import Carousel from 'react-elastic-carousel';
+import React, { useState } from "react";
+
+
+import Item from "./Item";
+
+const products = [
+    {
+      id: 1,
+      name: 'Basic Tee',
+      href: '#',
+      imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '$35',
+      color: 'Black',
+    },
+    {
+        id: 2,
+        name: 'Basic Tee',
+        href: '#',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+        imageAlt: "Front of men's Basic Tee in black.",
+        price: '$35',
+        color: 'Black',
+    },
+    {
+        id: 3,
+        name: 'Basic Tee',
+        href: '#',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+        imageAlt: "Front of men's Basic Tee in black.",
+        price: '$35',
+        color: 'Black',
+    },
+    {
+        id: 4,
+        name: 'Basic Tee',
+        href: '#',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+        imageAlt: "Front of men's Basic Tee in black.",
+        price: '$35',
+        color: 'Black',
+    },
+    {
+        id: 5,
+        name: 'Basic Tee',
+        href: '#',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+        imageAlt: "Front of men's Basic Tee in black.",
+        price: '$35',
+        color: 'Black',
+    },
+    // More products...
+  ]
+
+const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+    { width: 768, itemsToShow: 3 },
+    { width: 1200, itemsToShow: 4 }
+  ];
+
+const Carousl = ()=>{
+    const [items, setItems] = useState([1, 2, 3, 4, 5, 6, 7, 8]);
+
+    const addItem = () => {
+      const nextItem = Math.max(1, items.length + 1);
+      setItems([...items, nextItem]);
+    };
+  
+    const removeItem = () => {
+      const endRange = Math.max(0, items.length - 1);
+      setItems(items.slice(0, endRange));
+    };
+  
+    return(
+      
+        <Carousel breakPoints={breakPoints}>
+          {/* {items.map((item) => (
+            <Item key={item}>
+                {item}
+            </Item>
+          ))} */}
+          {
+              products.map(x=>(
+                  <Item>
+                      <img src={x.imageSrc}/>
+                  </Item>
+              ))
+          }
+        </Carousel>
+   
+    );
+}
+export default Carousl;
